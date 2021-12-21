1afb2b5fec999e490c53376b79af3c396d97d759
Danilo Tomasoni <tomasoni@cosbi.eu>
2021-12-21 14:51:53 +0100

    also principal component allows to select variables on which we want to operate

--- R/VisualSHIELDServer.R
+++ R/VisualSHIELDServer.R
@@ -1156,7 +1156,7 @@ VisualSHIELDServer <- function(id, servers, LOG_FILE="VisualSHIELD.log", glm_max
               )
             ), 
             shiny::conditionalPanel(
-              condition = paste0("input['",ns('plotType'),"'] == 'boxplot'"),
+              condition = paste0("input['",ns('plotType'),"'] == 'boxplot' || input['",ns('plotType'),"'] == 'princomp'"),
               
               shiny::selectInput(ns("vars_x"), "Variates",
                                  choices=varnames,
@@ -1550,7 +1550,9 @@ VisualSHIELDServer <- function(id, servers, LOG_FILE="VisualSHIELD.log", glm_max
               })
             }else if(input$plotType == "princomp"){
               cat(paste0(Sys.time(),"  ","User ",globalValues$username," is performing Principal Component Analysis (PCA) on current table..\n"), file=LOG_FILE, append=TRUE)
-              get.vars.as.numeric(o, 'D', 'Variables', row.names(vars), vars);
+              get.vars.as.numeric(o, 'D', 'Variables', 
+                                  input$vars_x,
+                                  vars);
               tryCatch({
                 princomp <- dsSwissKnifeClient::dssPrincomp(df='Variables', type="combine", 
                                                             center=T, scale=F,
